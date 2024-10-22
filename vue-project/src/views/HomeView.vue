63d5cdeb40bf869ae871f75dd2070ea4ca35e286
hongducct <duchongkhoan100@gmail.com>
2024-10-22 23:18:32 +0700

    Thay đổi trong file HomeView.vue, xóa 1 phần comment

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -134,12 +134,6 @@
                               </button>
                             </div>
 
-                            <!-- <button type="button" @click="handleAddToCart(selectedProduct)"
-                              :disabled="quantity > selectedProduct.stock"
-                              class="mt-6 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                              Thêm vào giỏ hàng
-                            </button> -->
-
                             <button type="button" @click="handleAddToCart" :disabled="quantity > selectedProduct.stock"
                               class="mt-6 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                               Thêm vào giỏ hàng
